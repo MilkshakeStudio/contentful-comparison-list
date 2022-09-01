2692aed087a66145fa4877fd47e18d999c204984
Simon Goetz <simon@milkshake.studio>
2022-09-01 11:58:52 -0400

    updated contentful comparison list

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
+  tabWidth: 3,
+  useTabs: true,
   singleQuote: true,
   jsxBracketSameLine: true
 };
--- src/index.js
+++ src/index.js
@@ -4,218 +4,197 @@ import ReactDOM from 'react-dom';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import { arrayMoveImmutable } from 'array-move';
 import { v4 } from 'uuid';
-import { Card, FormLabel, Button, Icon, TextInput, SelectField, Option } from '@contentful/forma-36-react-components';
+import {
+	Card,
+	FormLabel,
+	Button,
+	Icon,
+	TextInput,
+	SelectField,
+	Option,
+} from '@contentful/forma-36-react-components';
 import { init } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
 
-export const App = ({sdk}) => {
-  
-  //===============================================================/
-  //  CONTENT STATE
-  //===============================================================/
+export const App = ({ sdk }) => {
+	//===============================================================/
+	//  CONTENT STATE
+	//===============================================================/
 
-  const [listItems, setListItems] = useState( sdk.field.getValue() );
+   const startState = sdk.field.getValue() || {listArr:[]};
+	const [listItems, setListItems] = useState(startState);
 
+	//===============================================================/
+	//  CONTENT HANDLERS
+	//===============================================================/
 
-  //===============================================================/
-  //  CONTENT HANDLERS
-  //===============================================================/
+	// const onExternalChange = value => {
+	//   setListItems(value);
+	// }
 
-  // const onExternalChange = value => {
-  //   setListItems(value);
-  // }
+	const handleChange = (e) => {
+		const newList = listItems.listArr.slice();
 
-  const handleChange = (e) => {
-    const newList = listItems.listArr.slice()
-    for (let item of newList) {
-      if (e.target.id === item.id) {
-        e.target.name.includes("text") && (item.content = e.target.value)
-        e.target.name.includes("vowelOptions") && (item.vowelOption = e.target.value)
-        e.target.name.includes("competitorOptions") && (item.competitorOption = e.target.value)
-      }
-    }
-    setListItems(prevState => (
-      {
-        ...prevState,
-        listArr: newList
-      }
-    ))
-    sdk.field.setValue(listItems)
-  }
+		for (let item of newList) {
+			if (e.target.id === item.id) {
+				e.target.name.includes('text') && (item.content = e.target.value);
+				e.target.name.includes('vowelOptions') && (item.vowelOption = e.target.value);
+				e.target.name.includes('competitorOptions') && (item.competitorOption = e.target.value);
+			}
+		}
+		setListItems((prevState) => ({
+			...prevState,
+			listArr: newList,
+		}));
+      
+		sdk.field.setValue(listItems);
+	};
 
-  useEffect(() => {
-    sdk.window.startAutoResizer();
-  }, []);
+	useEffect(() => {
+		sdk.window.startAutoResizer();
+	}, []);
 
-  // useEffect(() => {
-  //   // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-  //   const detatchValueChangeHandler = sdk.field.onValueChanged(onExternalChange);
-  //   return detatchValueChangeHandler;
-  // });
+	// useEffect(() => {
+	//   // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+	//   const detatchValueChangeHandler = sdk.field.onValueChanged(onExternalChange);
+	//   return detatchValueChangeHandler;
+	// });
 
+	//===============================================================/
+	//  CARD HANDLERS
+	//===============================================================/
 
-  //===============================================================/
-  //  CARD HANDLERS
-  //===============================================================/
+	const handleAddItem = () => {
+		setListItems((prevState) => ({
+			...prevState,
+			listArr: [
+				...prevState.listArr,
+				{
+					id: v4(),
+					content: '',
+					vowelOption: 'yes',
+					competitorOption: 'no',
+				},
+			],
+		}));
+		sdk.field.setValue(listItems);
+	};
 
-  const handleAddItem = () => {
-    setListItems( prevState => (
-      {
-        ...prevState,
-        listArr: [
-          ...prevState.listArr,
-          {
-            id: v4(),
-            content: "",
-            vowelOption: "yes",
-            competitorOption: "no"
-          }
-        ]
-      }
-    ))
-    sdk.field.setValue(listItems)
-  }
+	const handleDeleteItem = (e) => {
+		const cardId = e.target.closest('.btn-remove').id;
+		const newList = listItems.listArr.slice();
+		for (let [index, item] of newList.entries()) {
+			item.id === cardId && newList.splice(index, 1);
+		}
+		setListItems((prevState) => ({
+			...prevState,
+			listArr: newList,
+		}));
+		sdk.field.setValue(listItems);
+	};
 
-  const handleDeleteItem = (e) => {
-    const cardId = e.target.closest(".btn-remove").id
-    const newList = listItems.listArr.slice()
-    for (let [index, item] of newList.entries()) {
-      item.id === cardId && newList.splice(index, 1)
-    }
-    setListItems( prevState => (
-      {
-        ...prevState,
-        listArr: newList
-      }
-    ))
-    sdk.field.setValue(listItems)
-  }
+	const handleReorder = ({ oldIndex, newIndex }) => {
+		const newList = listItems.listArr.slice();
+		const orderedList = arrayMoveImmutable(newList, oldIndex, newIndex);
+		setListItems((prevState) => ({
+			...prevState,
+			listArr: orderedList,
+		}));
+		sdk.field.setValue(listItems);
+	};
 
-  const handleReorder = ({oldIndex, newIndex}) => {
-    const newList = listItems.listArr.slice()
-    const orderedList = arrayMoveImmutable(newList, oldIndex, newIndex)
-    setListItems( prevState => (
-      {
-        ...prevState,
-        listArr: orderedList
-      }
-    ))
-    sdk.field.setValue(listItems)
-  }
+	//===============================================================/
+	//  SORTABLE COMPONENT
+	//===============================================================/
 
+	const SortableItem = SortableElement(({ data }) => {
+		return (
+			<li key={data.id} id={data.id}>
+				<Card className="list-item-wrapper">
+					<div className="drag-icon">
+						<Icon icon="Drag" />
+					</div>
+					<div className="list-item textarea">
+						<TextInput
+							name={`text-${data.id}`}
+							id={data.id}
+							value={data.content}
+							placeholder="what do you want to compare?"
+							onBlur={(e) => handleChange(e)}
+						/>
+					</div>
+					<SelectField
+						name={`vowelOptions-${data.id}`}
+						id={data.id}
+						labelText="Vowel:"
+						onChange={(e) => handleChange(e)}
+						value={data.vowelOption}>
+						<Option value="yes">Yes</Option>
+						<Option value="no">No</Option>
+						<Option value="sometimes">Sometimes</Option>
+						<Option value="question">Question</Option>
+					</SelectField>
+					<SelectField
+						name={`competitorOptions-${data.id}`}
+						id={data.id}
+						labelText="Competitor:"
+						onChange={(e) => handleChange(e)}
+						value={data.competitorOption}>
+						<Option value="yes">Yes</Option>
+						<Option value="no">No</Option>
+						<Option value="sometimes">Sometimes</Option>
+						<Option value="question">Question</Option>
+					</SelectField>
+					<div className="list-item btn-remove-wrapper">
+						<Button
+							buttonType="negative"
+							size="small"
+							icon="HorizontalRule"
+							id={data.id}
+							onClick={(e) => handleDeleteItem(e)}
+							className="btn-remove"></Button>
+					</div>
+				</Card>
+			</li>
+		);
+	});
 
-  //===============================================================/
-  //  SORTABLE COMPONENT
-  //===============================================================/
+	const SortableList = SortableContainer(() => {
+		return (
+			<ul className="list" key="main">
+				{listItems.listArr.map((item, i) => (
+					<SortableItem data={item} key={item.id} index={i} />
+				))}
+			</ul>
+		);
+	});
 
-  const SortableItem = SortableElement(({data}) => {
-    return (
-      <li 
-        key={data.id}
-        id={data.id}
-      >
-        <Card
-          className="list-item-wrapper"
-        >
-          <div className="drag-icon">
-            <Icon icon="Drag" />
-          </div>
-          <div className="list-item textarea">
-            <TextInput
-              name={`text-${data.id}`}
-              id={data.id}
-              value={data.content}
-              placeholder="what do you want to compare?"
-              onBlur={(e) => handleChange(e)}
-            />
-          </div>
-          <SelectField
-            name={`vowelOptions-${data.id}`}
-            id={data.id}
-            labelText="Vowel:"
-            onChange={(e) => handleChange(e)}
-            value={data.vowelOption}
-          >
-            <Option value="yes" >Yes</Option>
-            <Option value="no" >No</Option>
-            <Option value="sometimes" >Sometimes</Option>
-            <Option value="question" >Question</Option>
-          </SelectField>
-          <SelectField
-            name={`competitorOptions-${data.id}`}
-            id={data.id}
-            labelText="Competitor:"
-            onChange={(e) => handleChange(e)}
-            value={data.competitorOption}
-          >
-            <Option value="yes" >Yes</Option>
-            <Option value="no" >No</Option>
-            <Option value="sometimes" >Sometimes</Option>
-            <Option value="question" >Question</Option>
-          </SelectField>
-          <div className="list-item btn-remove-wrapper">
-            <Button
-              buttonType="negative"
-              size="small"
-              icon="HorizontalRule"
-              id={data.id}
-              onClick={(e) => handleDeleteItem(e)}
-              className="btn-remove"
-            ></Button>
-          </div>
-        </Card>
-      </li>
-    )
-  })
+	//===============================================================/
+	//  RENDERED COMPONENT
+	//===============================================================/
 
-  const SortableList = SortableContainer(() => {
-    return (
-      <ul
-        className="list"
-        key="main"
-      >
-        {listItems.listArr.map((item, i) => ( 
-          <SortableItem
-            data={item}
-            key={item.id}
-            index={i}
-          />
-        ))}
-      </ul>
-    )
-  })
-
-
-  //===============================================================/
-  //  RENDERED COMPONENT
-  //===============================================================/
-
-  return (
-    <>
-      <SortableList
-        onSortEnd={handleReorder}
-        lockAxis="y"
-        distance={15}
-        key="main"
-      />
-      <div className="btn-add-wrapper">
-        <Button
-          buttonType="positive"
-          size="small"
-          icon="Plus"
-          onClick={handleAddItem}
-          className="btn-add"
-        >add row</Button>
-      </div>
-    </>
-  );
-}
-
-App.propTypes = {
-  sdk: PropTypes.object.isRequired
+	return (
+		<>
+			<SortableList onSortEnd={handleReorder} lockAxis="y" distance={15} key="main" />
+			<div className="btn-add-wrapper">
+				<Button
+					buttonType="positive"
+					size="small"
+					icon="Plus"
+					onClick={handleAddItem}
+					className="btn-add">
+					add row
+				</Button>
+			</div>
+		</>
+	);
 };
 
-init(sdk => {
-  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+App.propTypes = {
+	sdk: PropTypes.object.isRequired,
+};
+
+init((sdk) => {
+	ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
 });
